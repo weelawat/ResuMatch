5c2fee5b7f2067abb577bc11a9db0d945f9bcd9b
Weelawawt Sampao <Weelewat@gmail.com>
2025-11-21 07:31:29 +0700

    Refactor database initialization by removing unused create_db_and_tables function and related async context manager from main.py. Simplified database setup in database.py.

--- src/app/database.py
+++ src/app/database.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel import create_engine, Session
 from src.app.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
@@ -6,9 +6,3 @@ engine = create_engine(settings.DATABASE_URL)
 def get_session():
     with Session(engine) as session:
         yield session
-
-def create_db_and_tables():
-    from src.app.models.role import RoleProfile
-    from src.app.models.candidate import Candidate
-    
-    SQLModel.metadata.create_all(engine)
